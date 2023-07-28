5019e89747b6c9432a46aee0309923ab6b0d2555
Mikhail <Mimintoff@mail.ru>
2023-07-28 22:28:01 +0300

    Create Project

---
+++ src/test/java/HomeWorks/PracticeForm.java
@@ -0,0 +1,29 @@
+package HomeWorks;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class PracticeForm {
+
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+
+    }
+    @Test
+    void fillStudentPracticeForm(){
+        open("https://demoqa.com/automation-practice-form");
+        $(".main-header").shouldHave(text("Practice Form"));
+
+
+    }
+
+}
