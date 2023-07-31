32a07d5ed2205943cd7a685b2da2584619bb9037
Mikhail <Mimintoff@mail.ru>
2023-07-31 22:00:43 +0300

    Create Test

--- src/test/java/HomeWorks/PracticeForm.java
+++ src/test/java/HomeWorks/PracticeForm.java
@@ -1,36 +1,91 @@
 package HomeWorks;
 
 import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeForm {
 
 
+
+
     @BeforeAll
-    static void beforeAll() {
+    static void setupClass() {
+        System.setProperty("webdriver.chrome.driver", "C:\\CODE");
+        WebDriverManager.chromedriver().cachePath("C:\\CODE").avoidOutputTree().setup();
+        Configuration.browser = "chrome";
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-
     }
+
+
+
+
     @Test
     void fillStudentPracticeForm(){
-        open("https://demoqa.com/automation-practice-form");
-        $(".main-header").shouldHave(text("Practice Form"));
-        $("#firstName").setValue("Михаил");
-        $("#lastName").setValue("Кочержинский");
-        $("#userEmail").setValue("Michelle908@mail.ru");
-        $("#userEmail").setValue("Michelle908@mail.ru");
-        $("#gender-radio-3").setValue("3");
-        $("#hobbies-checkbox-3").setValue("3");
-        $("#dateOfBirthInput").setValue("");
-        $("#subjectsContainer").setValue("");
+        String firstName = "Mikhail";
+        String lastName = "Kocherginski";
+        String email = "Michelle908@mail.ru";
+        String mobileNumber = "+79969308634";
+        String gender = "Male";
+        String birthDateDay = "20";
+        String month  = "October";
+        String year = "1995";
+        String subjects = "Maths";
+        String hobbies = "Music";
+        String currentAddress = "Kostroma";
+        String nameFile = "photo_2023-07-24_09-14-48.jpg";
+        String state =  "Haryana";
+        String city = "Karnal";
 
+
+        open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("#genterWrapper").$(byText(gender)).click();
+        $("#userNumber").setValue(mobileNumber);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-dropdown-container").click();
+        $(byText(year)).click();
+        $(".react-datepicker__month-dropdown-container").click();
+        $(byText(month)).click();
+        $(".react-datepicker__day--0" + birthDateDay).click();
+        $("#subjectsContainer input").setValue(subjects);
+        $(".subjects-auto-complete__menu").$(byText(subjects)).click();
+        $("#hobbiesWrapper").$(byText(hobbies)).click();
+        $("input#uploadPicture").uploadFile(new File("src/test/resources/" + nameFile));
+        $("#currentAddress").setValue(currentAddress);
+        $(byText("Select State")).click();
+        $(byText(state)).click();
+        $("#city").click();
+        $(byText(city)).click();
+        $("button#submit").click();
+
+
+        $$(".table-responsive tr").findBy(text("Student Name" + " " + firstName + " " + lastName)).shouldBe(visible);
+        $$(".table-responsive tr").findBy(text("Student Email" + " " + email)).shouldBe(visible);
+        $$(".table-responsive tr").findBy(text("Gender" + " " + gender)).shouldBe(visible);
+        $$(".table-responsive tr").findBy(text("Mobile" + " " + mobileNumber)).shouldBe(visible);
+        $$(".table-responsive tr").findBy(text("Date of Birth" + " " + birthDateDay + " " + month + "," + year)).shouldBe(visible);
+        $$(".table-responsive tr").findBy(text("Hobbies" + " " + hobbies)).shouldBe(visible);
+        $$(".table-responsive tr").findBy(text("Picture" + " " + nameFile)).shouldBe(visible);
+        $$(".table-responsive tr").findBy(text("Address" + " " + currentAddress)).shouldBe(visible);
+        $$(".table-responsive tr").findBy(text("State and City" + " " + state + " " + city)).shouldBe(visible);
+        $$(".table-responsive tr").findBy(text("Subjects" + " " + subjects)).shouldBe(visible);
 
 
     }
